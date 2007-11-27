b39710d1bf059ca3e8b20510bec2e46a930346dd
willuhn <willuhn>
2007-11-27 15:17:12 +0000

    @N CRC-Check und Bankname-Lookup

--- src/de/willuhn/jameica/hbci/xmlrpc/rmi/KontoService.java
+++ src/de/willuhn/jameica/hbci/xmlrpc/rmi/KontoService.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus.xmlrpc/src/de/willuhn/jameica/hbci/xmlrpc/rmi/KontoService.java,v $
- * $Revision: 1.2 $
- * $Date: 2006/11/07 00:18:11 $
+ * $Revision: 1.3 $
+ * $Date: 2007/11/27 15:17:12 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -40,11 +40,31 @@ public interface KontoService extends Service
    * @throws RemoteException
    */
   public String create(String kontonummer, String blz, String name, String kundennummer) throws RemoteException;
+  
+  /**
+   * Prueft eine BLZ/Kontonummer auf Plausibilitaet anhand der Pruefsumme.
+   * @param blz BLZ.
+   * @param kontonummer Kontonummer.
+   * @return true oder false.
+   * @throws RemoteException
+   */
+  public boolean checkAccountCRC(String blz, String kontonummer) throws RemoteException;
+  
+  /**
+   * Liefert den Namen des Kredit
+   * @param blz BLZ
+   * @return Name des Kreditinstitut.s
+   * @throws RemoteException
+   */
+  public String getBankname(String blz) throws RemoteException;
 }
 
 
 /*********************************************************************
  * $Log: KontoService.java,v $
+ * Revision 1.3  2007/11/27 15:17:12  willuhn
+ * @N CRC-Check und Bankname-Lookup
+ *
  * Revision 1.2  2006/11/07 00:18:11  willuhn
  * *** empty log message ***
  *
--- src/de/willuhn/jameica/hbci/xmlrpc/server/KontoServiceImpl.java
+++ src/de/willuhn/jameica/hbci/xmlrpc/server/KontoServiceImpl.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/hibiscus/hibiscus.xmlrpc/src/de/willuhn/jameica/hbci/xmlrpc/server/KontoServiceImpl.java,v $
- * $Revision: 1.3 $
- * $Date: 2007/07/06 13:21:18 $
+ * $Revision: 1.4 $
+ * $Date: 2007/11/27 15:17:13 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -21,6 +21,7 @@ import de.willuhn.datasource.rmi.DBService;
 import de.willuhn.jameica.hbci.HBCI;
 import de.willuhn.jameica.hbci.rmi.Konto;
 import de.willuhn.jameica.hbci.xmlrpc.rmi.KontoService;
+import de.willuhn.jameica.messaging.QueryMessage;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
@@ -130,11 +131,42 @@ public class KontoServiceImpl extends AbstractServiceImpl implements
     return "[xml-rpc] konto";
   }
 
+  /**
+   * @see de.willuhn.jameica.hbci.xmlrpc.rmi.KontoService#checkAccountCRC(java.lang.String, java.lang.String)
+   */
+  public boolean checkAccountCRC(String blz, String kontonummer) throws RemoteException
+  {
+    if (blz == null || kontonummer == null || blz.length() == 0 || kontonummer.length() == 0)
+      return false;
+    QueryMessage msg = new QueryMessage(blz + ":" + kontonummer);
+    Application.getMessagingFactory().getMessagingQueue("hibiscus.query.accountcrc").sendSyncMessage(msg);
+    Object value = msg.getData();
+    if (value == null || !(value instanceof Boolean))
+      return false;
+    return ((Boolean)value).booleanValue();
+  }
+
+  /**
+   * @see de.willuhn.jameica.hbci.xmlrpc.rmi.KontoService#getBankname(java.lang.String)
+   */
+  public String getBankname(String blz) throws RemoteException
+  {
+    if (blz == null || blz.length() == 0)
+      return "";
+    QueryMessage msg = new QueryMessage(blz);
+    Application.getMessagingFactory().getMessagingQueue("hibiscus.query.bankname").sendSyncMessage(msg);
+    Object value = msg.getData();
+    return value == null ? "" : value.toString();
+  }
+
 }
 
 
 /*********************************************************************
  * $Log: KontoServiceImpl.java,v $
+ * Revision 1.4  2007/11/27 15:17:13  willuhn
+ * @N CRC-Check und Bankname-Lookup
+ *
  * Revision 1.3  2007/07/06 13:21:18  willuhn
  * @N Saldo mit zurueckliefern
  *
